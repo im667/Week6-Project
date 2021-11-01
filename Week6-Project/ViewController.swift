fcadaef6f7c2d6d524a59eca1d965d16041a189c
kwon <koh9037@gmail.com>
2021-11-01 21:39:04 +0900

    Initial Commit

---
+++ Week6-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week6-Project
+//
+//  Created by mac on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
